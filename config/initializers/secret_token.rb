21fad77c29455e5aa779e51d1e83c553e84ecaef
G Scott Stukey <gscottstukey@gmail.com>
2013-08-03 14:59:37 -0700

    Initial Unlinked Load

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unlinked::Application.config.secret_token = '02e64d23fa34053ce0a6e164b91d26da7a7ecccc6d8538002f1c046abbb4c6c803ffa3d3a85772fc3fd90d52e2e4b6cc8fba122ec92270382fd0edfd16f52227'
